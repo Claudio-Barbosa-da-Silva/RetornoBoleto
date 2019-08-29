d019b0ee96cae18a597d490d9ff922f548350605
telecentro <telecentro@telecentro-06>
2019-08-28 22:19:14 -0300

    Implementação do boleto do Banco Bradesco

---
+++ src/main/java/Boleto.java
@@ -0,0 +1,110 @@
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author telecentro
+ */
+public class Boleto {
+    
+    public int id;
+    public String codBanco;
+    public LocalDate dataVencimento;
+    public LocalDateTime dataPagamento;
+    public String cpfCliente;
+    public double valor;
+    public double multa;
+    public double juros;
+    public String agencia;
+    public String contaBancaria;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCodBanco() {
+        return codBanco;
+    }
+
+    public void setCodBanco(String codBanco) {
+        this.codBanco = codBanco;
+    }
+
+    public LocalDate getDataVencimento() {
+        return dataVencimento;
+    }
+
+    public void setDataVencimento(LocalDate dataVencimento) {
+        this.dataVencimento = dataVencimento;
+    }
+
+    public LocalDateTime getDataPagamento() {
+        return dataPagamento;
+    }
+
+    public void setDataPagamento(LocalDateTime dataPagamento) {
+        this.dataPagamento = dataPagamento;
+    }
+
+    public String getCpfCliente() {
+        return cpfCliente;
+    }
+
+    public void setCpfCliente(String cpfCliente) {
+        this.cpfCliente = cpfCliente;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+
+    public double getMulta() {
+        return multa;
+    }
+
+    public void setMulta(double multa) {
+        this.multa = multa;
+    }
+
+    public double getJuros() {
+        return juros;
+    }
+
+    public void setJuros(double juros) {
+        this.juros = juros;
+    }
+
+    public String getAgencia() {
+        return agencia;
+    }
+
+    public void setAgencia(String agencia) {
+        this.agencia = agencia;
+    }
+
+    public String getContaBancaria() {
+        return contaBancaria;
+    }
+
+    public void setContaBancaria(String contaBancaria) {
+        this.contaBancaria = contaBancaria;
+    }
+    
+      
+}
+
---
+++ src/main/java/LeituraRetornoBradesco.java
@@ -0,0 +1,62 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author telecentro
+ */
+public class LeituraRetornoBradesco implements LeituraRetorno{
+
+    @Override
+    public List<Boleto> lerArquivo(String nomeArquivo) {
+        try {
+            BufferedReader reader = Files.newBufferedReader(Paths.get(nomeArquivo));
+            
+            String ln;
+
+            List<Boleto> boletos = new ArrayList<>();
+            
+            while((ln = reader.readLine())!= null){
+                String[] vetor = ln.split(";");
+                Boleto boleto = new Boleto();
+                boleto.setId(Integer.valueOf(vetor[0]));
+                boleto.setCodBanco(vetor[1]);
+                boleto.setDataVencimento(LocalDate.parse(vetor[2]));
+                boleto.setDataPagamento(LocalDateTime.parse(vetor[3]));
+                boleto.setCpfCliente(vetor[4]);
+                boleto.setValor(Double.valueOf(vetor[5]));
+                boleto.setMulta(Double.valueOf(vetor[6]));
+                boleto.setJuros(Double.valueOf(vetor[7]));
+                boleto.setAgencia(vetor[8]);
+                boleto.setContaBancaria(vetor[9]);
+                
+                boletos.add(boleto);
+                
+            }
+            return boletos;
+                    
+        } catch (IOException ex) {
+            Logger.getLogger(LeituraRetornoBradesco.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        return Collections.emptyList();
+    }
+    
+}
+
